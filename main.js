446b8192acfb055779547a40d67064702c02ac61
Jenny Allar <jennyallar@gmail.com>
2015-09-20 17:54:24 -0600

    Added 10 exercises

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+//THE OBJECT OF MY AFFECTION: OBJECTS
 //IN CLASS OBJECT EXERCISES
+
+// For any methods that we didn't cover, please refer to the Object Documentation.
+// Please console.log("Question#") followed by console.log(yourAnswer) after each question.
+// If you would like, practice using document.write() as well. Use any elements you like!
+// ==========================================================================
+
+// 1. Create an object called "me" that describes you with your name, city, and job title.
+
+//console.log("Question 1");
+//console.log(yourAnswer);
+
+// 2. To your "me" object, add a key of "family" with the value being an array of three of your family members.
+// Example of desired output: { name: 'Bob Smith',
+//                              city: 'Denver',
+//                              jobTitle: 'professional wrestler',
+//                               family: ['Joan', 'Kyle', 'Hank'] }
+
+// 3. Using the "me" object, use two different methods of accessing your city.
+
+// 4. Create an empty object called "jsWorkshop" using Constructor Notation.
+
+// 5. To your "jsWorkshop" object, assign a key of "classmates" with the value being an array of five your classmates.
+
+// 6. Acesss the third classmate in your "jsWorkshop" object.
+
+// 7. Get the length of the fourth classmate's name.
+
+// 8. Clear the classmates value.
+
+// 9. Create an array called "books" that contains three objects details book titles, their author, and number of pages.
+// Example of desired output: [{title: "You Had Me at Woof", author: "Julie Klum", pages: 207},
+//                             {title: "Bangkok Haunts", author: "John Burdett", pages: 558},
+//                             {title: "The Catcher in the Rye", author: "J.D. Salinger", pages: 214}]
+// (Feel free to make up the titles. And the authors. Have fun. No pressure.)
+
+// 10. To you array of objects variable, add a key of "dateRead" with a value of the year you read the book.
+// (Go ahead and make this up, too. Get wild.)
