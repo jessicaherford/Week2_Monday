0ffbece506b94e2a666fb607979eb3d6a8ae3f4c
Jenny Allar <jennyallar@gmail.com>
2015-09-20 19:07:50 -0600

    Refining and adding object exercises

--- main.js
+++ main.js
@@ -19,21 +19,90 @@
 
 // 3. Using the "me" object, use two different methods of accessing your city.
 
-// 4. Create an empty object called "jsWorkshop" using Constructor Notation.
+// Use the following Object for questions 4-9:
+var invitees = { adults: [{
+                          name: "Penny",
+                          table: 10,
+                          dinnerChoice: ["salad", "beef", "ice cream"]
+                        },
+                        {
+                          name: "Billy",
+                          table: 12,
+                          dinnerChoice: ["soup", "beef", "sorbet"]
+                        },
+                        {
+                          name: "Lauren",
+                          table: 3,
+                          dinnerChoice: ["soup", "chicken", "ice cream"],
+                          specialConsiderations: { allergies: "melon",
+                                                   seating: "Don't sit her next to ex husband at table 8."}
+                        }
+                        ],
+                  children: [{
+                             name: "Junior",
+                             table: 4,
+                             allergies: ["nuts"]
+                            },
+                            { name: "Pablo",
+                            table: 4,
+                            allergies: ["gluten", "following directions"]
+                            },
+                            { name: "Ada",
+                            table: 4,
+                            allergies: ["boys"]
+                            }
+                         ]
+                      };
 
-// 5. To your "jsWorkshop" object, assign a key of "classmates" with the value being an array of five your classmates.
+// 4. Access Penny's dinner choice.
 
-// 6. Acesss the third classmate in your "jsWorkshop" object.
+// 5. Access Pablo's table number.
 
-// 7. Get the length of the fourth classmate's name.
+// 6. Access Lauren's seating specialConsideration.
 
-// 8. Clear the classmates value.
+// 7. Access Billy's desert choice (this will be the third item in his dinneChoice array)
 
-// 9. Create an array called "books" that contains three objects details book titles, their author, and number of pages.
-// Example of desired output: [{title: "You Had Me at Woof", author: "Julie Klum", pages: 207},
-//                             {title: "Bangkok Haunts", author: "John Burdett", pages: 558},
-//                             {title: "The Catcher in the Rye", author: "J.D. Salinger", pages: 214}]
+// 8. Access Ada's allergies.
+
+// 9. Create a string that reads Lauren's dinner choice in a sentence.
+// Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
+// and has selected ________ as her dessert."
+
+// 10. Create an empty object called "jsWorkshop" using Constructor Notation.
+
+// 11. To your "jsWorkshop" object, assign a key of "classmates" with the value being an array of five your classmates.
+
+// 12. Acesss the third classmate in your "jsWorkshop" object.
+
+// 13. Get the length of the fourth classmate's name.
+
+// 14. Clear the classmates value.
+
+// 15. Create an emtpy array called "books".
+// Create three objects that contain book titles, their author, and number of pages.
+// Insert the three objects into the "books" array.
 // (Feel free to make up the titles. And the authors. Have fun. No pressure.)
+// Example of desired array: [ youHadMeAtWoof, bangkokHaunts, theCatcherInTheRye]
+// Example of desired object: {title: "You Had Me at Woof", author: "Julie Klum", pages: 207}
+// Final desired result:
+    var books = [{ youHadMeAtWoof: {
+                                  title: "You Had Me at Woof",
+                                  author: "Julie Klum",
+                                  pages: 207
+                                  },
+                   theCatcherInTheRye: {
+                                  title: "The Catcher in the Rye",
+                                  author: "JD Salinger",
+                                  pages: 214
+                                  },
+                   bangkokHaunts: {
+                                  title: "Bangkok Haunts",
+                                  author: "John Burdett",
+                                  pages: 569,
+                                  }
+                }]
 
-// 10. To you array of objects variable, add a key of "dateRead" with a value of the year you read the book.
+// 16. To you array of objects variable, add a key of "dateRead" with a value of the year you read the book.
 // (Go ahead and make this up, too. Get wild.)
+
+// Bonus: Explain, in your own words, the difference between Literal Notation and Constructor.
